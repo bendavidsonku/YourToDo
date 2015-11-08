b03ee1383ae686e28eea8410cb63955fe4729f13
Ben Davidson <bendavidson@ku.edu>
2015-11-08 00:38:06 -0600

    Registration Form now recognizes duplicate username/email and persists necessary data to DB

--- YourToDo/forms.py
+++ YourToDo/forms.py
@@ -2,20 +2,38 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+class UniqueUserEmailField(forms.EmailField):
+    """
+    An EmailField which only is valid if no User has that email.
+    """
+    def validate(self, value):
+        super(forms.EmailField, self).validate(value)
+        try:
+            User.objects.get(email = value)
+            raise forms.ValidationError("The specified email already belongs to a registered user")
+        except User.MultipleObjectsReturned:
+            raise forms.ValidationError("The specified email already belongs to a registered user")
+        except User.DoesNotExist:
+            pass
+
 class defaultUserRegistrationForm(UserCreationForm):
-	email = forms.EmailField(required = True)
-	firstName = forms.CharField(label = "First Name", required = True)
-	lastName = forms.CharField(label = "Last Name", required = True)
+	username = forms.CharField(label = 'Username', max_length=30, help_text=('Length: 30 characters or fewer. Letters, digits and '
+                    '@/./+/-/_ only.'), widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+	email = UniqueUserEmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'E-mail address'}))
+	first_name = forms.CharField(label = "First Name", required = True,  widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+	last_name = forms.CharField(label = "Last Name", required = True,  widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+	password1 = forms.CharField(label = "Password", required = True,  widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+	password2 = forms.CharField(label = "Password confirmation", required = True,  widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
 
 	class Meta:
 		model = User
-		fields = ('username', 'firstName', 'lastName', 'email', 'password1', 'password2')
+		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
 		def save(self, commit = True):
 			user = super(defaultUserRegistrationForm, self).save(commit = False)
 			user.email = self.cleaned_data['email']
-			user.firstName = self.cleaned_data['firstName']
-			user.lastName = self.cleaned_data['lastName']
+			user.firstName = self.cleaned_data['first_name']
+			user.lastName = self.cleaned_data['last_name']
 
 			if commit:
 				user.save()
--- YourToDo/views.py
+++ YourToDo/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.core.context_processors import csrf
+from django.template import RequestContext
+
 from YourToDo.forms import defaultUserRegistrationForm
 
 def login(request):
@@ -34,18 +36,19 @@ def logout(request):
     return render_to_response('auth/logout.html')
 
 def registration(request):
-    if request.method == 'POST':
-        form = defaultUserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/registration_success/')
-
     args = {}
     args.update(csrf(request))
 
-    args['form'] = defaultUserRegistrationForm()
+    if request.method == 'POST':
+        form = defaultUserRegistrationForm(request.POST)
+        args['form'] = form
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/registration_success/')
+    else:
+        args['form'] = defaultUserRegistrationForm()
 
-    return render_to_response('auth/registration.html', args)
+    return render_to_response('auth/registration.html', args, context_instance=RequestContext(request))
 
 def registration_success(reqest):
         return render_to_response('auth/registration_success.html')
