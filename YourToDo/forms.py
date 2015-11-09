ae008d19dbe39915772288e107cd01a5f9ee3a86
bendavidsonku <bendavidson@ku.edu>
2015-11-08 22:58:44 -0600

    Styled registration page and registration success page with other minor updates to wording

--- YourToDo/forms.py
+++ YourToDo/forms.py
@@ -20,7 +20,7 @@ class defaultUserRegistrationForm(UserCreationForm):
 	first_name = forms.CharField(required = True,  widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
 	last_name = forms.CharField(required = True,  widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
 	password1 = forms.CharField(required = True,  widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-	password2 = forms.CharField(required = True,  widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+	password2 = forms.CharField(required = True,  widget=forms.PasswordInput(attrs={'placeholder': 'Re-type Password'}))
 
 	class Meta:
 		model = User
