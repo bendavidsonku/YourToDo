1610ce501d65e256040cd8c5cdaf947f4e8d4c8c
bendavidson <bendavidson@ku.edu>
2015-11-11 00:21:12 -0600

    Login page now redirects to itself and displays errors and the action of logging in takes users directly back to home page for the time being.

--- YourToDo/forms.py
+++ YourToDo/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 class UniqueEmailField(forms.EmailField):
    
@@ -36,3 +37,7 @@ class defaultUserRegistrationForm(UserCreationForm):
 				user.save()
 
 			return user
+
+class defaultAuthenticationForm(AuthenticationForm):
+	username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
