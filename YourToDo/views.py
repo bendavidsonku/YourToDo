4db07bc4511a21295b96f61a3e8d5182521d00a0
bendavidson <bendavidson@ku.edu>
2015-11-14 13:59:08 -0600

    Got rid of all custom auth views and implemented view so logout returns user to homepage

--- YourToDo/views.py
+++ YourToDo/views.py
@@ -6,8 +6,6 @@ from django.core.context_processors import csrf
 from django.template import RequestContext
 from django.core.mail import send_mail
 
-from YourToDo.forms import defaultUserRegistrationForm
-from YourToDo.forms import defaultAuthenticationForm
 from YourToDo.forms import ContactForm
 
 # Static Page Views
@@ -39,44 +37,8 @@ def contact(request):
 def contact_success(request):
         return render_to_response('static/contact_success.html')
 
-# Authentication Views
-def login(request):
-    args = {}
-    args.update(csrf(request))
+def ytdLogout(request):
+	auth.logout(request)
+	return render_to_response('base.html')
 
-    if request.method == 'POST':
-        form = defaultAuthenticationForm(data = request.POST)
-        args['form'] = form
-        if form.is_valid():
-            username = request.POST.get('username', '')
-            password = request.POST.get('password', '')
-            user = auth.authenticate(username = username, password = password)
-            if user is not None:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('home'))
-    else:
-        args['form'] = defaultAuthenticationForm()
 
-    return render_to_response('auth/login.html', args, context_instance=RequestContext(request))
-
-def logout(request):
-    auth.logout(request)
-    return render_to_response('auth/logout.html')
-
-def registration(request):
-    args = {}
-    args.update(csrf(request))
-
-    if request.method == 'POST':
-        form = defaultUserRegistrationForm(request.POST)
-        args['form'] = form
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/registration_success/')
-    else:
-        args['form'] = defaultUserRegistrationForm()
-
-    return render_to_response('auth/registration.html', args, context_instance=RequestContext(request))
-
-def registration_success(reqest):
-        return render_to_response('auth/registration_success.html')
