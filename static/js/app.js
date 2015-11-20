19a259ad7a7c756b531ea2787a01c08145abf389
Jay Offerdahl <jaysanco@gmail.com>
2015-11-20 01:36:50 -0600

    Add date selectors to planner header & introduce session storage.
    Also added several styles to improve the layout of the date selectors & layout selectors, as well as a few improvements/fixes to the miniCal function.

--- static/js/app.js
+++ static/js/app.js
@@ -8,12 +8,109 @@ $(document).ready(function() {
             $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut();
         }
     );
+
+
+    // Session Storage
+    if (!(sessionStorage.viewDate)) {
+        var now = new Date();
+        sessionStorage.viewDate = now;
+    }
+
+    // Local Storage
+    if (!(localStorage.layoutType)) {
+        localStorage.layoutType = "week";
+    }
+
+    changeViewDate("FIRST", 0);
 });
 
+function getViewDate() {
+    return new Date(Date.parse(sessionStorage.viewDate));
+}
 
+/*
+    Changes the current viewDate in sessionStorage based on input:
 
-// Stores the number of days in the months of the year (January = 0...)
-var month_length = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    @param size
+        The size of the change: DAY, WEEK, MONTH, YEAR
+    @param amount
+        The amount of the change: -1, or 1, preferrably. (1 week, 1 month, -1 week, etc.)
+
+    Prompts all date fields to update to the new viewDate
+    Prompts the mini calendar to re-write itself if it's displayed.
+*/
+function changeViewDate(size, amount) {
+    var viewDate = getViewDate();
+
+    if(size === "DAY") {
+        viewDate.setDate(viewDate.getDate() + amount);
+    }
+    else if(size === "WEEK") {
+        viewDate.setDate(viewDate.getDate() + 7 * amount);
+    }
+    else if(size === "MONTH") {
+        viewDate.setMonth(viewDate.getMonth() + amount);
+    }
+    else if(size === "YEAR") {
+        viewDate.setFullYear(viewDate.getFullYear() + amount);
+    }
+    else if(size === "FIRST") {
+        // Do nothing, just running first time set up.
+    }
+    else {
+        console.log("changeViewDate() unknown parameter: " + size);
+    }
+
+    sessionStorage.viewDate = viewDate;
+
+    // Update all fields in case they changed
+    document.getElementById("planner-date-month-selector").innerHTML = month_names[viewDate.getMonth()];
+    document.getElementById("planner-date-week-selector").innerHTML = getWeekString();
+    document.getElementById("planner-date-year-selector").innerHTML = viewDate.getFullYear();
+
+    // #TODO show year on planner somewhere
+
+    // If the mini calendar is on the page (day & week views), update it
+    // Don't update it if this is the first time running this function
+    if(localStorage.layoutType != "month" && size != "FIRST") {
+        this.miniCal.handleDateChange();
+    }
+}
+
+// Store the month lengths & names
+var month_length = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+    month_names = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July',
+                   'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'],
+    month_names_long = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+                   'August', 'September', 'October', 'November', 'December'];
+
+// Do not ask me to explain this function because I don't want to spend an hour re-learning it.
+this.getWeekString = function() {
+    var viewDate = getViewDate(),
+        month = viewDate.getMonth() + 1;
+        // Start weekSunday at the current day - today's week index
+        weekSunday = viewDate.getDate() - viewDate.getDay(),
+        // Logically, weekSaturday is 6 days away from weekSunday
+        weekSaturday = weekSunday + 6,
+        // Assume the same month
+        monthNum1 = monthNum2 = month;
+        
+    // If weekSunday is negative, it goes into the previous month
+    if(weekSunday <= 0) {
+        monthNum1 = month == 1 ? 12 : month - 1;
+        // weekSunday still holds the overflow, so use it to get the sunday date from previous month
+        weekSunday = month_length[monthNum1 - 1] + weekSunday;
+    }
+
+    // If weekSaturday is > the month length of this month, it extends into following month
+    if(weekSaturday > month_length[month - 1]) {
+        weekSaturday -= month_length[month - 1];
+        monthNum2 = month == 12 ? 1 : month + 1;
+    }
+
+    // Return a string in the followin format: "7/1 - 7/7"
+    return monthNum1 + "/" + weekSunday + " - " + monthNum2 + "/" + weekSaturday;
+}
 
 /*  
     Fills in the mini-calendar specified table within the planner layout.
@@ -24,16 +121,20 @@ var month_length = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
         The month of the year (January = 0, December = 11)
     @param day
         The day of the week (Monday = 0, Sunday = 6)
+
+    @note
+        Use no parameters to use the current viewDate stored in sessionStorage
 */
-function miniCal(year, month, day) {
-    var now = new Date();
+this.miniCal = function(year, month, day) {
+    viewDate = getViewDate();
 
     // Initialize our time variable with the input if it's valid.
-    this.day = (isNaN(day) || day == null) ? now.getDate() : day;
-    this.month = (isNaN(month) || month == null) ? now.getMonth() : month;
-    this.year = (isNaN(year) || year == null) ? now.getFullYear() : year;
+    this.day = (isNaN(day) || day == null) ? viewDate.getDate() : day;
+    this.month = (isNaN(month) || month == null) ? viewDate.getMonth() : month;
+    this.year = (isNaN(year) || year == null) ? viewDate.getFullYear() : year;
     this.html = "";
 
+    // Returns the html (assuming it has been previously generated)
     this.getHTML = function() {
         return this.html;
     }
@@ -54,37 +155,26 @@ function miniCal(year, month, day) {
             currentDay = new Date(this.year, this.month, this.day),
             firstDayInt = firstDay.getDay(),
             currentDayInt = currentDay.getDay(),
+            currentDay = 1; // Use this to track where we are in the month
             lastDayInt = lastDay.getDay();
 
         // Variable to store the html that we'll write to the document
         var html = '<table class="planner-mini-calendar">' +
                         '<!--  7 equally spaced columns  -->' +
                         '<col><col><col><col><col><col><col>';
-        /*
-            Strategy: We know the first day, the month length and where the first day is in the week.
-
-            So, first, populate the boxes to the left of the first day (if any).
-            Populate the boxes of the month, and highlight the current week.
-
-            Lastly, populate the boxes to the right of the last day (if any).
-        */
 
         // Store how many days to the left of day 1, and the last number we need from those.
         var prevDays = firstDayInt == 0 ? 7 : firstDayInt,
             postDays = lastDayInt == 6 ? 7: 6 - lastDayInt;
 
-        // Track where we are in the month
-        var currentDay = 1;
-
-        // Track where we need to start in the previous month
-        var preMonth = this.month == 0 ? 12 : this.month - 1,
-            preCurrentDay = month_length[preMonth] - prevDays + 1;
-
+        // Find the integer value of the day in the previous month we need to start counting at
+        var preCurrentDay = month_length[this.month == 0 ? 11 : this.month - 1] - prevDays + 1;
+        
         // Loop for 6 weeks always (max number needed for the longest month)
         for(var calWeek = 0; calWeek < 6; calWeek++) {
             // If we're in the current week, highlight it.
-            if( this.day - currentDayInt == currentDay 
-                || (calWeek == 0 && this.day - currentDayInt < 1)) {
+            if((this.day - currentDayInt == currentDay && prevDays != 7) ||
+               (calWeek == 0 && this.day - currentDayInt < 1)) {
                 html += '<tr class="planner-mini-calendar-active">';
             }
             else {
@@ -111,7 +201,7 @@ function miniCal(year, month, day) {
                     html += '<td class="planner-mini-calendar-inactive">' + (currentDay - monthLength) + '</td>';
                     currentDay++;
                 }
-                // current month filler days
+                // If we're not filling pre or post days, that means we're still filling current month days
                 else {
                     html += '<td>' + currentDay + '</td>';
                     currentDay++;
@@ -120,12 +210,20 @@ function miniCal(year, month, day) {
             html += '</tr>';
         }
         html += '</table>';
+
+        // Store the generated html in our variable defined at class level
         this.html = html;
     }
 
-    // Updates the calendar to the current date objects. 
-    // #TODO connect this with the "current time" user attribute.
+    // Updates the calendar to the current view date
     this.handleDateChange = function() {
-        
+        viewDate = getViewDate();
+
+        this.day = viewDate.getDate();
+        this.month = viewDate.getMonth();
+        this.year = viewDate.getFullYear();
+
+        this.generateHTML();
+        document.getElementById("planner-mini-calendar").innerHTML = this.getHTML();
     }
 } 
